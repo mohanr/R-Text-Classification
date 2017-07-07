560f324977f7e49228b4b2e6ec974ea7ab645e65
476458 <476458@LT032871>
2017-07-07 18:06:33 +0530

    Extract classes and chat messages

--- server.R
+++ server.R
@@ -2,16 +2,41 @@ library(shiny)
 library(stringr)
 require(XML)
 
+readposts <- 
+		function(){
+			posts <- xmlParse("10-26-teens_706posts.xml",  ,useInternalNodes = TRUE)
+		}
+
+posts <- readposts()
+
+filteredclasses <-
+		function(){
+			classes <- as.data.frame(xpathSApply(doc = posts, path = "/*/Posts/Post/@class"))
+			print(sprintf("There are %d classes",length(unlist(classes))))
+}
+
+filteredmessagesandclasses <-
+		function(){
+			classframe <- filteredclasses()
+			messages <- xpathSApply(doc = posts, path = "//Post//text()",xmlValue)
+			print(sprintf("There are %d messages",length(messages)))
+			messageframe <- as.data.frame(messages)
+			messagesandclasses <- merge(messageframe,classframe)
+			messagesandclasses <-  messagesandclasses[ -which( messagesandclasses$messages==""), ]
+			print(sprintf("Merged frame has %d messages",length(messagesandclasses)))
+			print(colnames(messagesandclasses))
+			#messageframe$messages <- str_trim(messageframe$messages) 
+			#print( messagesandclasses )
+}
+
+
 
 function(input, output) {
 
-  posts <- xmlParse("10-26-teens_706posts.xml",  ,useInternalNodes = TRUE)
-  messages <- xpathSApply(doc = posts, path = "//Post//text()",xmlValue)
-  messageframe <- as.data.frame(messages)
-  messageframe$messages <- str_trim(messageframe$messages) 
-  print( messageframe )
+  filteredmessagesandclasses()
   output$text1 <- renderText({ 
 			  "class"
 		  })
 }
+
 
